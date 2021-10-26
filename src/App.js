40d89ff45a3aa5fea77896828386853b806470b3
Anuppandey009 <anuppandey000@gmail.com>
2021-10-26 23:30:42 +0530

    Stop watch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import {Stopwatch} from "./Components/Stopwatch"
+
+export default function App() {
+  return (
+    <div>
+      <h1> Stopwatch</h1>
+      <Stopwatch />
+     
+      </div>
+  )
+}
